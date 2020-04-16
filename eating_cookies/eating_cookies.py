b55c658244fe3353293b1658a328232e1abb246d
Vanessa <vanessatellez45@gmail.com>
2020-04-16 18:19:42 -0400

    eating cookies and rock-paper-scissors complete

--- eating_cookies/eating_cookies.py
+++ eating_cookies/eating_cookies.py
@@ -1,16 +1,87 @@
 #!/usr/bin/python
 
 import sys
+# Cookie Monster can eat either 0, 1, 2, or 3 cookies at a time.
+# Implement a function `eating_cookies` that counts the number of possible ways
+# Cookie Monster can eat all of the cookies in the jar.
 
 # The cache parameter is here for if you want to implement
-# a solution that is more efficient than the naive 
+# a solution that is more efficient than the naive
 # recursive solution
+
+"""
 def eating_cookies(n, cache=None):
-  pass
+    # if 1 or less cookies (0 or negative), only 1 way to eat it.
+    if n <= 1:
+        return 1
+    if n == 2:
+        return 2
+    else:
+        return eating_cookies(n-1) + eating_cookies(n-2) + eating_cookies(n-3)
+--------------------------------------
+
+(1)
+--------------
+1
+
+(2)
+---------------
+1  1
+2
+
+(3)
+---------------
+1  11
+1   2
+21
+3
+
+(4)
+----------------
+1 111
+1  12
+1  21
+1   3
+211
+22
+31
+
+(5)
+----------------
+1     1111
+1      112
+1      121
+1       13
+1      211
+1       22
+1       31
+2111
+221
+212
+23
+311
+32
+
+"""
+
+
+def eating_cookies(n, cache=None):
+    if cache is None:
+        cache = [0] * (n + 1)
+    if n <= 1:
+        cache[n] = 1
+    elif n == 2:
+        cache[n] = 2
+    elif cache[n] == 0:
+        cache[n] = eating_cookies(
+            n-1, cache) + eating_cookies(n-2, cache) + eating_cookies(n-3, cache)
+    return cache[n]
+
 
 if __name__ == "__main__":
-  if len(sys.argv) > 1:
-    num_cookies = int(sys.argv[1])
-    print("There are {ways} ways for Cookie Monster to eat {n} cookies.".format(ways=eating_cookies(num_cookies), n=num_cookies))
-  else:
-    print('Usage: eating_cookies.py [num_cookies]')
+    if len(sys.argv) > 1:
+        num_cookies = int(sys.argv[1])
+        print("There are {ways} ways for Cookie Monster to eat {n} cookies.".format(
+            ways=eating_cookies(num_cookies), n=num_cookies))
+    else:
+        print('Usage: eating_cookies.py [num_cookies]')
--- rock_paper_scissors/rps.py
+++ rock_paper_scissors/rps.py
@@ -1,14 +1,73 @@
 #!/usr/bin/python
 
+# function to generate all of the possible plays that can be made in a game of "Rock Paper Scissors"
+# Your output should be: a list of lists containing strings.
+#   Each inner list should have length equal to the input n.
+
+# * You'll want to define a list with all of the possible Rock Paper Scissors plays.
+# * The problem asks to generate a bunch of permutations, so we're probably going to want to opt for using recursion again.
+#   Since we're building up a list of results, we'll have to pass the list we're constructing around to multiple recursive
+#   calls so that each recursive call can add to the overall result. However, the tests only give our function `n` as input.
+#   To get around this, we could define an inner recursive helper function that will perform the recursion for us,
+#   while allowing us to preserve the outer function's function signature.
+
+# * In Python, you can concatenate two lists with the `+` operator.
+#   However, you'll want to make sure that both operands are lists!
+
+# * If you opt to define an inner recursive helper function,
+#   don't forget to make an initial call to the recursive helper function to kick off the recursion.
+
 import sys
+"""
+(0)
+------------
+[[]]
+
+(1)
+------------
+[['rock'], ['paper'], ['scissors']]
+
+(2)
+-------------
+[['rock', 'rock'], ['rock', 'paper'], ['rock', 'scissors'],
+ ['paper', 'rock'], ['paper', 'paper'], ['paper', 'scissors'], 
+ ['scissors', 'rock'], ['scissors', 'paper'], ['scissors', 'scissors']]
+
+ (3)
+ ------------
+[['rock', 'rock', 'rock'], ['rock', 'rock', 'paper'], ['rock', 'rock', 'scissors'],
+ ['rock', 'paper', 'rock'], ['rock', 'paper', 'paper'], ['rock', 'paper', 'scissors'],
+ ['rock', 'scissors', 'rock'], ['rock', 'scissors', 'paper'], ['rock', 'scissors', 'scissors'],
+ ['paper', 'rock', 'rock'], ['paper', 'rock', 'paper'], ['paper', 'rock', 'scissors'],
+ ['paper', 'paper', 'rock'], ['paper', 'paper', 'paper'], ['paper', 'paper', 'scissors'],
+ ['paper', 'scissors', 'rock'], ['paper', 'scissors', 'paper'], ['paper', 'scissors', 'scissors'],
+ ['scissors', 'rock', 'rock'], ['scissors', 'rock', 'paper'], ['scissors', 'rock', 'scissors'],
+ ['scissors', 'paper', 'rock'], ['scissors', 'paper', 'paper'], ['scissors', 'paper', 'scissors'],
+ ['scissors', 'scissors', 'rock'], ['scissors', 'scissors', 'paper'], ['scissors', 'scissors', 'scissors']
+
+"""
+
 
 def rock_paper_scissors(n):
-  pass 
+    output = []
+    rps = [['rock'], ['paper'], ['scissors']]
 
+    if n <= 0:
+        return [[]]
+    if n == 1:
+        return rps
+    else:
+        for i in rock_paper_scissors(n-1):
+            for j in rps:
+                output.append(i + j)
+    return output
+
+
+print(rock_paper_scissors(3))
 
 if __name__ == "__main__":
-  if len(sys.argv) > 1:
-    num_plays = int(sys.argv[1])
-    print(rock_paper_scissors(num_plays))
-  else:
-    print('Usage: rps.py [num_plays]')
+    if len(sys.argv) > 1:
+        num_plays = int(sys.argv[1])
+        print(rock_paper_scissors(num_plays))
+    else:
+        print('Usage: rps.py [num_plays]')
